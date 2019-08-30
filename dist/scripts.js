5209ff1a02f76d363d0aeabad5b6b24c7f560fb5
unscodst <spikester@sbcglobal.net>
2019-08-30 15:57:01 -0700

    Typescript Configuration: Single File
    Restructuring compiling to create a single .js file in a ./dist folder. This will allow me to create as many typescript files as I want and only include one .js file on the html page.

--- dist/scripts.js
+++ dist/scripts.js
@@ -12,3 +12,19 @@ button.onclick = function () {
     alert(greeter.greet());
 };
 document.body.appendChild(button);
+// String
+var myName = 'Matt';
+// myName = 28;
+// Number
+var myAge = 28;
+//myAge = 'Matt';
+// Boolean
+var hasHobbies = true;
+// hasHobbies = 1;
+// Assign types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = '27';
+// Array
+var hobbies = ["Cooking", "Sports"];
+console.log(hobbies[0]);
---
+++ typescript/types.ts
@@ -0,0 +1,20 @@
+// String
+let myName:string = 'Matt';
+// myName = 28;
+
+// Number
+let myAge:number = 28;
+//myAge = 'Matt';
+
+// Boolean
+let hasHobbies:boolean = true;
+// hasHobbies = 1;
+
+// Assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+// Array
+let hobbies = ["Cooking", "Sports"];
+console.log(hobbies[0]);
